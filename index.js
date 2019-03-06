1d579a46b860e9bdecaf4eacb3681c1995c96c02
PatAkil <patrickakil@hotmail.com>
2019-03-06 16:32:45 +0100

    Redux boilerplate

--- index.js
+++ index.js
@@ -3,7 +3,7 @@
  * @lint-ignore-every XPLATJSCOPYRIGHT1
  */
 
-import App from './src/App';
+import App from './src/app';
 import {AppRegistry} from 'react-native';
 import {name as appName} from './app.json'
 
